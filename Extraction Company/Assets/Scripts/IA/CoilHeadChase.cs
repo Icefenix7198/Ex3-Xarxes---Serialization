3b24676a23bf17750a27467dfe57ebdc2a4c8f37
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2024-11-22 00:02:41 +0100

    Step 0 chase coilhead

---
+++ Extraction Company/Assets/Scripts/IA/CoilHeadChase.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoilHeadChase : State
+{
+
+    public override State RunCurrentState()
+    {
+        return this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private bool IsInView(GameObject origin, GameObject toCheck)
+    {
+        Vector3 pointOnScreen = origin.GetComponentInChildren<Camera>().WorldToScreenPoint(toCheck.GetComponentInChildren<Renderer>().bounds.center);
+
+        //Is in front
+        if (pointOnScreen.z < 0)
+        {
+            Debug.Log("Behind: " + toCheck.name);
+            return false;
+        }
+
+        //Is in FOV
+        if ((pointOnScreen.x < 0) || (pointOnScreen.x > Screen.width) ||
+                (pointOnScreen.y < 0) || (pointOnScreen.y > Screen.height))
+        {
+            Debug.Log("OutOfBounds: " + toCheck.name);
+            return false;
+        }
+
+        RaycastHit hit;
+        Vector3 heading = toCheck.transform.position - origin.transform.position;
+        Vector3 direction = heading.normalized;// / heading.magnitude;
+
+        if (Physics.Linecast(origin.transform.position, toCheck.GetComponentInChildren<Renderer>().bounds.center, out hit))
+        {
+            if (hit.transform.name != toCheck.name)
+            {
+                /* -->
+                Debug.DrawLine(cam.transform.position, toCheck.GetComponentInChildren<Renderer>().bounds.center, Color.red);
+                Debug.LogError(toCheck.name + " occluded by " + hit.transform.name);
+                */
+                Debug.Log(toCheck.name + " occluded by " + hit.transform.name);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    
+}
