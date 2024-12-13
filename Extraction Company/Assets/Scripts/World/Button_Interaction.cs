74b5a42069e32857c19994c7558ed4ba33e3dd3b
SoniaOL <99950351+SoniaOL@users.noreply.github.com>
2024-12-14 00:34:27 +0100

    intento puertas

--- Extraction Company/Assets/Scripts/World/Button_Interaction.cs
+++ Extraction Company/Assets/Scripts/World/Button_Interaction.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class Button_Interaction : MonoBehaviour
 {
     public bool button;
+    public List<Animator> animator;
+
+    public bool buttonA;
+    public bool buttonB;
+    public bool buttonC;
 
     // Start is called before the first frame update
     void Start()
     {
-        button = true;
+        button = false;
+        buttonA = false;
+        buttonB = false;
+        buttonC = false;
     }
 
     // Update is called once per frame
@@ -24,18 +32,89 @@ public class Button_Interaction : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.E))
         {
-            if(button == true)
+            if(this.CompareTag("GroupA"))
             {
-                rend.material.SetColor("_Color", Color.red);
-                rend.material.SetColor("_EmissionColor", Color.red);
-                button = false; 
+                if (buttonA == true)
+                {
+                    rend.material.SetColor("_Color", Color.red);
+                    rend.material.SetColor("_EmissionColor", Color.red);
+                    buttonA = false;
+
+                }
+                else if (buttonA == false)
+                {
+                    rend.material.SetColor("_Color", Color.green);
+                    rend.material.SetColor("_EmissionColor", Color.green);
+                    buttonA = true;
+                }
             }
 
-            else
+            if (this.CompareTag("GroupB"))
             {
-                rend.material.SetColor("_Color", Color.green);
-                rend.material.SetColor("_EmissionColor", Color.green);
-                button = true; 
+                if (buttonB== true)
+                {
+                    rend.material.SetColor("_Color", Color.red);
+                    rend.material.SetColor("_EmissionColor", Color.red);
+                    buttonB = false;
+
+                }
+                else if (buttonB == false)
+                {
+                    rend.material.SetColor("_Color", Color.green);
+                    rend.material.SetColor("_EmissionColor", Color.green);
+                    buttonB = true;
+                }
+            }
+
+            if (this.CompareTag("GroupC"))
+            {
+                if (buttonC == true)
+                {
+                    rend.material.SetColor("_Color", Color.red);
+                    rend.material.SetColor("_EmissionColor", Color.red);
+                    buttonC = false;
+
+                }
+                else if (buttonC == false)
+                {
+                    rend.material.SetColor("_Color", Color.green);
+                    rend.material.SetColor("_EmissionColor", Color.green);
+                    buttonC = true;
+                }
+            }
+
+
+            for (int i = 0; i < animator.Count; i++) {
+
+                if (animator[i].CompareTag("GroupA")) { 
+                    animator[i].SetBool("Button", buttonA); 
+                }
+
+                else
+                {
+                    animator[i].SetBool("Button", !buttonA); 
+                }
+
+                if (animator[i].CompareTag("GroupB"))
+                {
+                    animator[i].SetBool("Button", buttonB);
+                }
+
+                else
+                {
+                    animator[i].SetBool("Button", !buttonB);
+                }
+
+                if (animator[i].CompareTag("GroupC"))
+                {
+                    animator[i].SetBool("Button", buttonC);
+                }
+
+                else
+                {
+                    animator[i].SetBool("Button", !buttonC);
+                }
+
             }
         }
     }
